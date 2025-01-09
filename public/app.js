58d699fc68f1e38034db609e2c2c23698c9859db
bowie <marketing@letscode.com.br>
2025-01-09 03:01:16 -0300

    first commit

---
+++ public/app.js
@@ -0,0 +1,75 @@
+const videos = [
+  {
+    nome: "Deadpool & Wolverine",
+    url: "/video1/343rt342wtg34wetg34retg4rghy5rh/FHD4/tt6263850.mp4",
+  },
+  {
+    nome: "Venom: A Última Rodada",
+    url: "/video1/343rt342wtg34wetg34retg4rghy5rh/FHD4/tt16366836.mp4",
+  },
+  {
+    nome: "DivertidaMente 2",
+    url: "/video1/343rt342wtg34wetg34retg4rghy5rh/FHD4/tt22022452.mp4",
+  },
+  {
+    nome: "A Liga",
+    url: "/video1/343rt342wtg34wetg34retg4rghy5rh/FHD4/tt12610390.mp4",
+  },
+  {
+    nome: "Spaceman",
+    url: "/video2/movie/swilder2023/6063815781/96792.mp4",
+  },
+  {
+    nome: "Kung Fu Panda 4 (2024)",
+    url: "/video2/movie/swilder2023/6063815781/100551.mp4",
+  },
+  {
+    nome: "Spaceman",
+    url: "/video2/movie/swilder2023/6063815781/96792.mp4",
+  },
+];
+
+function criarListaDeVideos() {
+  const videoList = document.getElementById("videoList");
+  videos.forEach((video) => {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    button.className = "list-button";
+    button.setAttribute("data-src", video.url);
+    button.innerText = video.nome;
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      const videoSrc = this.getAttribute("data-src");
+      carregarIframe(videoSrc);
+    });
+    li.appendChild(button);
+    videoList.appendChild(li);
+  });
+}
+
+criarListaDeVideos();
+
+function carregarIframe(url) {
+  var videoPlayer = document.getElementById("videoPlayer");
+  videoPlayer.style.display = "block";
+  videoPlayer.src = null;
+  if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource('/proxy?url=' + encodeURIComponent(url));
+    hls.attachMedia(videoPlayer);
+    hls.on(Hls.Events.MANIFEST_PARSED, function () {
+      videoPlayer.play();
+    });
+  } else if (videoPlayer.canPlayType("application/vnd.apple.mpegurl")) {
+    videoPlayer.src = '/proxy?url=' + encodeURIComponent(url);
+    videoPlayer.addEventListener("canplay", function () {
+      videoPlayer.play();
+    });
+  } else {
+    videoPlayer.src = '/proxy?url=' + encodeURIComponent(url);
+    videoPlayer.play();
+  }
+}
+
+
+
---
+++ server.js
@@ -0,0 +1,97 @@
+const https = require("https");
+const http = require("http");
+const express = require("express");
+const selfsigned = require("selfsigned");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+// Gerar certificados autoassinados
+const attrs = [{ name: "commonName", value: "localhost" }];
+const pems = selfsigned.generate(attrs, { days: 365 });
+
+// Configurar CORS
+app.use(cors());
+
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// Rotas
+app.get("/", (req, res) => {
+  const inputFilePath = path.join(__dirname, "public/index.html");
+  fs.readFile(inputFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading the input file:", err);
+      res.status(500).send("Error reading the input file");
+      return;
+    }
+    res.send(data);
+  });
+});
+
+// Configurar o proxy para vídeos HTTP do primeiro domínio
+app.use(
+  "/video1",
+  createProxyMiddleware({
+    target: "http://fhd4.doramasapp.xyz",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/video1": "", // Remove o prefixo /video1 da URL
+    },
+  })
+);
+
+// Configurar o proxy para vídeos HTTP do segundo domínio
+app.use(
+  "/video2",
+  createProxyMiddleware({
+    target: "http://tkosportz.live:25461",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/video2": "", // Remove o prefixo /video2 da URL
+    },
+  })
+);
+
+// Configurar o proxy para redirecionar qualquer URL
+app.use(
+  "/proxy",
+  createProxyMiddleware({
+    target: "",
+    changeOrigin: true,
+    router: function (req) {
+      const targetUrl = new URL(req.query.url);
+      return targetUrl.origin;
+    },
+    pathRewrite: function (path, req) {
+      const targetUrl = new URL(req.query.url);
+      return targetUrl.pathname + targetUrl.search;
+    },
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    },
+    logLevel: "debug",
+  })
+);
+
+// Servidor HTTPS
+https
+  .createServer({ key: pems.private, cert: pems.cert }, app)
+  .listen(443, () => {
+    console.log("HTTPS server running on port 443");
+  });
+
+// Redirecionamento HTTP para HTTPS
+http
+  .createServer((req, res) => {
+    res.writeHead(301, {
+      Location: "https://" + req.headers["host"] + req.url,
+    });
+    res.end();
+  })
+  .listen(80, () => {
+    console.log("HTTP server running on port 80 and redirecting to HTTPS");
+  });
